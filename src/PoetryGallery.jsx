34e4bd7817ce4939dcdc7fa6c2d03bd9185c26e7
Henrique O. <lemonheadch@outlook.com>
2026-05-09 10:38:39 -0300

    Update PoetryGallery.jsx

--- src/PoetryGallery.jsx
+++ src/PoetryGallery.jsx
@@ -27,7 +27,7 @@ const poems = [
   { id: 6, img: "/poems/poem6-5.png", audio: "/audios/poem6-5.mp3" },
   { id: 7, img: "/poems/poem7-5.png", audio: "/audios/poem7-5.mp3" },
   { id: 8, img: "/poems/poem8-5.png", audio: "/audios/poem8-5.mp3" },
-  /*{ id: 9, img: "/poems/poem9-5.png", audio: "/audios/poem9-5.mp3" },*/
+  { id: 9, img: "/poems/poem9-5.png", audio: "/audios/poem9-5.mp3" },
   /*{ id: 10, img: "/poems/poem10-5.png", audio: "/audios/poem10-5.mp3" },*/
   /*{ id: 11, img: "/poems/poem11-5.png", audio: "/audios/poem11-5.mp3" },*/
   /*{ id: 12, img: "/poems/poem12-5.png", audio: "/audios/poem12-5.mp3" },*/
