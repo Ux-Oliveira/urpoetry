32c6655eb0280ef35bb7e7356f10a9f2bfa624b1
Henrique O. <lemonheadch@outlook.com>
2026-06-05 12:23:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import PoetryGallery from "./PoetryGallery";
 import Game from "./Game.jsx";
+import Bday from "./Bday.jsx";
 
 const poetryContainer = document.getElementById("poetry-root");
 
@@ -13,4 +14,10 @@ const gameContainer = document.getElementById("game-root");
 
 if (gameContainer) {
   createRoot(gameContainer).render(<Game />);
+}
+
+const bdayContainer = document.getElementById("bday-root");
+
+if (bdayContainer) {
+  createRoot(bdayContainer).render(<Bday />);
 }
